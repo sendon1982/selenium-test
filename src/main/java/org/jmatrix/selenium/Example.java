22560c1df350c7bebfebfb3e503943195733ce58
mjiang <mjiang@viator.com>
2015-12-11 20:07:02 +1100

    test

--- src/main/java/org/jmatrix/selenium/Example.java
+++ src/main/java/org/jmatrix/selenium/Example.java
@@ -1,16 +1,22 @@
 package org.jmatrix.selenium;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
-public class Example  {
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Example {
     public static void main(String[] args) {
         // Create a new instance of the html unit driver
         // Notice that the remainder of the code relies on the interface,
         // not the implementation.
-        WebDriver driver = new HtmlUnitDriver();
+        WebDriver driver = new FirefoxDriver();
 
         // And now use this to visit Google
         driver.get("http://www.google.com");
@@ -28,5 +34,21 @@ public class Example  {
         System.out.println("Page title is: " + driver.getTitle());
 
         driver.quit();
+    }
+
+    FirefoxDriver driver = null;
+    String baseUrl = null;
+
+    @Before
+    public void setUp() throws Exception {
+        //driver = new HtmlUnitDriver();
+        driver = new FirefoxDriver();
+        baseUrl = "http://www.google.com";
+//        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testDemo() throws Exception {
+        driver.get(baseUrl + "/");
     }
 }
